cfe39b003dba076b3be48efd6a7ef24d1678be5b
John Pisokas <john.pisokas@gmail.com>
2020-12-06 17:06:51 +0000

    Update watch script to accept more options

--- watch_script_warn_email.sh
+++ watch_script_warn_email.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-file="${1}" && last_mod_date=$(stat --printf="%Y" "${file}") && now_date=$(date +%s) && diff=$((${now_date} - ${last_mod_date})) && [ ${diff} -ge 600 ] && echo "Complete file ${file} at $(date)" | mailx -s "Task complete: Eddie simulations finished" s0093128@ed.ac.uk && exit
+
+if [ "$#" -lt 1 ]; then
+   echo "ERROR: Expects at least one argument."
+   echo "Sends me an email if a file has not been modified for 300s or for the specified number of seconds."
+   echo
+   echo "USAGE:"
+   echo "watch -n 60 $0 <FILE_TO_WATCH> <CHECK_EVERY_X_SEC>"
+   exit 1
+fi
+
+file="${1}" && last_mod_date=$(stat --printf="%Y" "${file}") && now_date=$(date +%s) && diff=$((${now_date} - ${last_mod_date})) && [ ${diff} -ge ${2:-600} ] && echo "Complete file ${file} at $(date)" | mailx -s "Task complete: Eddie simulations finished" s0093128@ed.ac.uk && exit
