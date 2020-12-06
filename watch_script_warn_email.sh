6cd3248aeabe1f33f96c7dc05517a1e7a634f533
John Pisokas <john.pisokas@gmail.com>
2020-12-06 17:15:09 +0000

    Update watch script to accept more options

--- watch_script_warn_email.sh
+++ watch_script_warn_email.sh
@@ -2,10 +2,15 @@
 
 if [ "$#" -lt 1 ]; then
    echo "ERROR: Expects at least one argument."
+   echo
    echo "Sends me an email if a file has not been modified for 300s or for the specified number of seconds."
    echo
    echo "USAGE:"
-   echo "watch -n 60 $0 <FILE_TO_WATCH> <CHECK_EVERY_X_SEC>"
+   echo "watch -n 60 $0 <FILE_TO_WATCH> [CHECK_EVERY_X_SEC]"
+   echo
+   echo "  <FILE_TO_WATCH>     The file to be watching for changes."
+   echo "  [CHECK_EVERY_X_SEC] Test file for modification every so "
+   echo "                      many seconds. Default 600s."
    exit 1
 fi
 
