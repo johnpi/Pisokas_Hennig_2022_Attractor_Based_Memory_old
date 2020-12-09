51ea3b3deb962f6665949a176a62e49185897a49
John Pisokas <john.pisokas@gmail.com>
2020-12-09 21:27:09 +0000

    Added argument to keep only the first x entities from each file

--- merge_all_files_into_one.py
+++ merge_all_files_into_one.py
@@ -29,7 +29,7 @@ merged_data_filename = 'Data/collected_drift_trials_v3and4_all.npy'
 
 
 
-def merge_file(output_file, input_files_list):
+def merge_file(output_file, input_files_list, max_entities=None):
     """
         Reads and combines all the data records from multiple files 
         and stores them in a new file.
@@ -48,10 +48,16 @@ def merge_file(output_file, input_files_list):
             collected_trials_data_input = np.load(collected_data_file, allow_pickle=True, encoding='bytes')
         except: 
             collected_trials_data_input = np.array([]) # Collected trials data records list
-
-        for i,item in enumerate(collected_trials_data_input):
-            # Add new data record to the collected trials data
-            collected_trials_data = np.append(collected_trials_data, item)
+        
+        if max_entities is not None and len(collected_trials_data_input) > max_entities:
+            for i in xrange(max_entities):
+                item = collected_trials_data_input[i]
+                # Add new data record to the collected trials data
+                collected_trials_data = np.append(collected_trials_data, item)
+        else:
+            for i,item in enumerate(collected_trials_data_input):
+                # Add new data record to the collected trials data
+                collected_trials_data = np.append(collected_trials_data, item)
     
     # Save all data in the file
     np.save(output_file, collected_trials_data, allow_pickle=True)
@@ -70,13 +76,16 @@ parser.add_argument('-f', '--file', type=str, dest='output_file', required=True,
                    help='Output filename to write all collected data to.')
 parser.add_argument('-i', '--input-files', type=str, nargs='+', dest='input_files', required=True,
 help='One or more filename of .npy files to read data from and combine them into one output file.')
+parser.add_argument('-m', '--max', type=int, dest='max_entities', required=False, default=None, 
+help='Keep only the first [max] entries from each file and merge them to the output file.')
 
 # Parse the command line arguments
 args = parser.parse_args()
 
 input_files = args.input_files
 output_file = args.output_file
+max_entities = args max_entities
 
 # Was
 # merge_file(merged_data_filename, collected_data_files_list)
-merge_file(output_file, input_files)
+merge_file(output_file, input_files, max_entities)
